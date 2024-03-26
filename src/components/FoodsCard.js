b24919b61846df69adcbd481c4b715a9bbee6b7d
roid zuhdianto <roidzuh@gmail.com>
2024-03-26 11:00:26 +0700

    tambah link ke halaman detail

--- src/components/FoodsCard.js
+++ src/components/FoodsCard.js
@@ -1,8 +1,18 @@
-export default function FoodsCard({ imageUrl, name }) {
+import Link from "next/link";
+
+export default function FoodsCard({ imageUrl, name, id }) {
+  console.log(id);
   return (
-    <div className="">
-      <img src={imageUrl} alt={name} className="w-80 h-80 object-cover" />
-      <p>{name}</p>
+    <div className="flex flex-col items-center gap-3">
+      <img
+        src={imageUrl}
+        alt={name}
+        className="w-56 h-56 rounded-lg object-cover "
+      />
+      <div>
+        <p>{name}</p>
+        <Link href={`/food/${id}`}>Detail</Link>
+      </div>
     </div>
   );
 }

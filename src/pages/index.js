78e1325618370589d8a747a10596edc37ea9df49
roid zuhdianto <roidzuh@gmail.com>
2024-03-26 08:49:03 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const route = useRouter();

585eed9ff9343f63f5c68353d6b5ed2b457e4755
roid zuhdianto <roidzuh@gmail.com>
2024-03-27 11:41:19 +0700

    update food card

---
+++ src/components/FoodCard.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function FoodCard({ imageUrl, name, id }) {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <img
+        src={imageUrl}
+        alt={name}
+        className="w-56 h-56 rounded-lg object-cover "
+      />
+      <div>
+        <p>{name}</p>
+        <Link href={`/food/${id}`}>Detail</Link>
+      </div>
+    </div>
+  );
+}

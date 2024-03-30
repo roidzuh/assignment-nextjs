754e87e41fbdc7f36d247365252ba88d579d43e3
roid zuhdianto <roidzuh@gmail.com>
2024-03-30 12:23:36 +0700

    update styling

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -2,15 +2,13 @@ import Link from "next/link";
 
 export default function FoodCard({ imageUrl, name, id }) {
   return (
-    <div className="flex flex-col items-center gap-3">
-      <img
-        src={imageUrl}
-        alt={name}
-        className="w-56 h-56 rounded-lg object-cover "
-      />
-      <div>
-        <p>{name}</p>
-        <Link href={`/food/${id}`}>Detail</Link>
+    <div className="bg-gray-100 shadow-lg rounded-lg overflow-hidden">
+      <img src={imageUrl} alt={name} className="w-full h-56 object-cover" />
+      <div className="p-4">
+        <p className="text-xl font-semibold mb-2">{name}</p>
+        <Link href={`/food/${id}`} className="text-blue-500 hover:underline">
+          Detail
+        </Link>
       </div>
     </div>
   );

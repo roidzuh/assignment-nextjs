88ba1a8d285e94f705120af6e23d89c0e7b62261
roid zuhdianto <roidzuh@gmail.com>
2024-03-30 12:24:46 +0700

    update styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <div className="">Header</div>;
+  return (
+    <header className="py-4 bg-white fixed top-0 left-0 right-0 z-10 shadow-md">
+      <div className="container mx-auto flex justify-between items-center cursor-pointer">
+        <h1 className="text-4xl font-bold text-blue-500 hover:text-blue-800">
+          FOODIDU
+        </h1>
+        <nav>
+          <Link
+            href={"/food/create"}
+            className="text-blue-500 hover:text-blue-800"
+          >
+            Create Food
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
 }
